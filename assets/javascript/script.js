7210223efa31a2eb9c1d7eb575f55bac4f1abb43
Anthony James Manenti <anthonyj.manenti@gmail.com>
2018-04-13 00:53:37 -0400

    integration.2.0

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,41 @@
 
-
-
-
-  window.onload = function() {
-
-    var searchThis =  [ $("#userInput").val(''),
-                        $(this).data('search') ]
-  
-  
-    var queryURL = "https://developer.nps.gov/api/v1/parks?=stateCode=" + searchThis + "&api_key=7wQNlZMqMhlH0js2AdSZsiMoge4n3Z0ud2rZVlfW"
-
- 
-
+$(document).ready(function () {
+  //Click Handler for Random Park Search
+  $("#pickRandomPark").on("click", function (event) {
+    console.log("click handler working");
+    event.preventDefault();
+    $('#insertParkInfoHere').empty();
+    // //Pick A Random Number Between 1-60
+    var randomSelect = Math.floor(Math.random() * 60);
+    console.log(randomSelect); //number in console
+    // //parkCode Array
+    var parkCodes = ["acad", "npsa", "arch", "badl", "bibe", "bisc", "blca", "brca", "cany", "care", "cave", "chis", "cong", "crla", "cuva", "deva", "dena", "drto", "ever", "gaar", "jeff", "glac", "glba", "grca", "grta", "grba", "grsa", "grsm", "gumo", "hale", "havo", "hosp", "isro", "jotr", "katm", "kefj", "seki", "kova", "lacl", "lavo", "maca", "meve", "mora", "noca", "olym", "pefo", "pinn", "redw", "romo", "sagu", "shen", "thro", "viis", "voya", "wica", "wrst", "yell", "yose", "zion"];
+    console.log(parkCodes);
+    //Gives us a Random Park Code
+    var selectedCode = parkCodes[randomSelect]
+    console.log(selectedCode);
+    // API Query
+    var queryQRL = "https://developer.nps.gov/api/v1/parks?parkCode=" + selectedCode + "&api_key=7wQNlZMqMhlH0js2AdSZsiMoge4n3Z0ud2rZVlfW";
+    console.log(queryQRL);
+    //  
+    var pickRandom = parkCodes[Math.floor(Math.random() * parkCodes.length)];
+    console.log(pickRandom);
+   
     $.ajax({
-            url: queryURL,
-            method: "GET"
-          
-          }).done(function(response) {
-            var results = response.data;
-            console.log(results);
-            $("#resultsField").empty();
-            for (var i = 0; i < results.length; i++) {
-            var displayDiv = $("<div class='col-md-4'>"); }
-          });
-
+      url: "https://developer.nps.gov/api/v1/parks?parkCode=" + selectedCode + "&api_key=7wQNlZMqMhlH0js2AdSZsiMoge4n3Z0ud2rZVlfW",
+      method: "GET"
+    }).done(function (response) {
+      console.log('response.data[0]', response.data[0]);
+      $('#insertParkInfoHere').empty();
+      selectedParkName = response.data[0].fullName;
+      console.log('the random park is: ' + selectedParkName );
+      $('#insertParkInfoHere').append('<h2 id="responseTitle">' + "Name of National Park: " + '</h2><span id="responseInfo">' + response.data[0].fullName + '</span>');
+      $('#insertParkInfoHere').append('<h2 id="responseTitle">' + "Location: " + '</h2><span id="responseInfo">' + response.data[0].states + '</span>');
+      $('#insertParkInfoHere').append('<h2 id="responseTitle">' + "Latitude & Longitude: " + '</h2><span id="responseInfo">' + response.data[0].latLong + '</span>');
+      $('#insertParkInfoHere').append('<h2 id="responseTitle">' + "Description: " + '</h2><span id="responseInfo">' + response.data[0].description + '</span>');
+      $('#insertParkInfoHere').append('<h2 id="responseTitle">' + "Weather: " + '</h2><span id="responseInfo">' + response.data[0].weatherInfo + '</span>');
+    });
+  });
 
 
           var config = {
@@ -66,34 +78,47 @@
               });
 
 
-  $('.grid').packery({
-    // options
-    itemSelector: '.grid-item',
-    gutter: 10
-  });
+              $("#browseStateBtn").on("click", function (event) {
+                //prevents on click from resorting to default state
+                event.preventDefault();
+                $('#insertParkInfoHere').empty();
+                //get value from state dropdown
+                //var selectedState = $('#browseStateOption option:selected').text();
+              var selectedState = $('#browseStateOption').val()
+                console.log(selectedState);
+               
+               // stateCode object
+                // var stateCodeObject = {
+                //   "Alabama": AL, "Alaska": AK, "Arizona": AZ, "Arkansa": AR, "California": CA, "Colorado": CO, "Conneticut": CT, "Delaware": DE, "Florida": FL, "Georgia": GA, "Hawaii": HI, "Idaho": ID, "Illinois": IL, "Indiana": IN, "Iowa": IA, "Kansas": KS, "Kentucky": KY, "Louisiana": LA, "Maine": ME, "Maryland": MD, "Massachusetts": MA, "Michigan": MI, "Minnesota": MN, "Mississippi": MS, "Missouri": MO, "Montana": MT, "Nebraska": NE, "Nevada": NV, "New Hampshire": NH, "New Jersey": NJ, "New Mexico": NM, "New York": NY, "North Carolina": NC, "North Dakota": ND, "Ohio": OH, "Oklahoma": OK, "Oregon": OR, "Pennsylvania": PA, "Rhode Island": RI, "South Carolina": SC, "South Dakota": SD, "Tennessee": TN, "Texas": TX, "Utah": UT, "Vermont": VT, "Virginia": VA, "Washington": WA, "West Virginia": WV, "Wisconson": WI, "Wyoming": WY
+                // }
+            
+                // // API QUERY FOR STATE CODES FROM NPS
+                 var queryQRL = "https://developer.nps.gov/api/v1/parks?="+selectedState + "&api_key=7wQNlZMqMhlH0js2AdSZsiMoge4n3Z0ud2rZVlfW"
+                // //console.log of API Query
+                 console.log(queryQRL);
+              });
+            });
+            
+            const emptyNPS = () => {
+              console.log('in emptyNPS');
+              $('#insertSearchFormhere').empty();
+              $('#insertParkInfoHere').empty();
+            }
 
-  
-};
+       
+// external js: packery.pkgd.js, jquery-ui-draggable.js
 
-$("#pickRandomPark").on("click", function(event) {
+// initialize Packery
+var $grid = $('.grid').packery({
+  itemSelector: '.grid-item',
+  // columnWidth helps with drop positioning
+  columnWidth: 100
+});
 
-  var randomSelect=Math.floor(Math.random() * 60);
-console.log(randomSelect);
-var parkCodes = ["acad","npsa","arch","badl","bibe","bisc","blca","brca","cany","care","cave","chis","cong","crla","cuva", "deva","dena","drto","ever","gaar","jeff","glac","glba","grca","grta","grba","grsa","grsm","gumo","hale","havo","hosp","isro","jotr","katm","kefj","seki","kova","lacl","lavo","maca","meve","mora","noca","olym","pefo","pinn","redw","romo","sagu","shen","thro","viis","voya","wica","wrst","yell","yose","zion"];
-  var selectedCode=parkCodes[randomSelect]
-var queryQRL = "https://developer.nps.gov/api/v1/parks?parkCode=" + selectedCode + "&api_key=7wQNlZMqMhlH0js2AdSZsiMoge4n3Z0ud2rZVlfW";
-console.log(queryQRL);
-var pickRandom = function randomSelect() {
-    for (var i = 0; i < parkcodes.length; i++) {
-    var randomSelect=Math.floor(Math.random() * 60); 
-    var selectedCode=parkCodes[randomSelect]
-var queryQRL = "https://developer.nps.gov/api/v1/parks?parkCode" + selectedCode + "&api_key=7wQNlZMqMhlH0js2AdSZsiMoge4n3Z0ud2rZVlfW";
-console.log(queryQRL);
-var selectedCode=parkCodes[randomSelect] 
+// make all items draggable
+var $items = $grid.find('.grid-item').draggable();
+// bind drag events to Packery
+$grid.packery( 'bindUIDraggableEvents', $items );
 
-    }
-  }
-    
-    });
 
    
