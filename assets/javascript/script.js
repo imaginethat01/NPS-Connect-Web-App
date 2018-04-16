03fe8018ecf579c0e11358aa4f3de97c946d1319
Anthony James Manenti <anthonyj.manenti@gmail.com>
2018-04-15 21:44:10 -0400

    expand functional design. implement practical features. cosmetic updates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -163,6 +163,15 @@ $("#keywordSearchBtn").on("click", function (event) {
      $('#insertGooglehere').append(' <iframe width="550" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCESN4bg_XY8N82CwU7ssef7snFR53K1rY&q=' + place + '" allowfullscreen></iframe >'); 
     });
 
+
+  function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+  }
+
     var modal = document.getElementById('id01'); 
         
         
